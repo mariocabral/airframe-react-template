813ea954c24daaefb2582f266e3eb782f629ca87
mario <mlc-lab@protonmail.com>
2020-10-09 15:29:27 -0300

    remove unused files

--- app/layout/components/DefaultNavbar.js
+++ app/layout/components/DefaultNavbar.js
@@ -24,25 +24,6 @@ export const DefaultNavbar = () => (
                     <LogoThemed />
                 </Link>
             </NavItem>
-            <NavItem className="d-none d-md-block">
-                <span className="navbar-text">
-                    <Link to="/">
-                        <i className="fa fa-home"></i>
-                    </Link>
-                </span>
-                <span className="navbar-text px-2">
-                    <i className="fa fa-angle-right"></i>
-                </span>
-                <span className="navbar-text">
-                    <Link to="/">Start</Link>
-                </span>
-                <span className="navbar-text px-2">
-                    <i className="fa fa-angle-right"></i>
-                </span>
-                <span className="navbar-text">
-                    Page Link
-                </span>
-            </NavItem>
         </Nav>
         <Nav navbar className="ml-auto">
             <NavbarActivityFeed />
